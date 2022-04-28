d0118e8ffb07190af721beeb6fe449faa1c3be59
Marco Marinho <marcoobom@gmail.com>
2022-04-28 19:21:28 +0200

    2021 - Day 20

--- 2021/src/bin/day20.rs
+++ 2021/src/bin/day20.rs
@@ -4,10 +4,52 @@ use std::ops;
 
 fn main() {
     let (enhancements, image) = get_scanners();
-    let nimage = increase_frame(&image, false, 5);
+    let nimage = increase_frame(&image, false, 55);
     print_image(&nimage);
-    let val = get_pixel_value(&nimage, 7, 7);
-    println!("{}", val);
+    let nimage = enhance(&nimage, &enhancements, false, 2);
+    print_image(&nimage);
+    let num_lit = count_lit_pixels(&nimage);
+    println!("Task 01: {}", num_lit);
+    let nimage = enhance(&nimage, &enhancements, false, 48);
+    print_image(&nimage);
+    let num_lit = count_lit_pixels(&nimage);
+    println!("Task 02: {}", num_lit);
+}
+
+fn enhance(image: &Vec<Vec<bool>>, enhancements: &Vec<bool>, default: bool, times: u64) -> Vec<Vec<bool>>{
+    let mut default = default;
+    let mut image = image.clone();
+    for _ in 0 .. times {
+        image = enhance_image(&image, enhancements, default);
+        default = !default;
+    }
+    image
+}
+
+fn count_lit_pixels(image: &Vec<Vec<bool>>) -> u64{
+    let nrows = image.len();
+    let ncols = image[0].len();
+    let mut output = 0;
+    for i_row in 0 .. nrows{
+        for i_col in 0 .. ncols {
+            if image[i_row][i_col] { output += 1; }
+        }
+    }
+    output
+}
+
+fn enhance_image(image: &Vec<Vec<bool>>, enhancements: &Vec<bool>, default: bool) -> Vec<Vec<bool>>{
+    let nrows = image.len();
+    let ncols = image[0].len();
+    let mut nimage = image.clone();
+    for i_row in 0 .. nrows{
+        for i_col in 0 .. ncols {
+            let pixel_val = get_pixel_value(image, i_row, i_col, default);
+            let pixel = enhancements[pixel_val];
+            nimage[i_row][i_col] = pixel;
+        }
+    }
+    nimage
 }
 
 fn print_image(image: &Vec<Vec<bool>>){
@@ -15,16 +57,23 @@ fn print_image(image: &Vec<Vec<bool>>){
         let buff: String = row.into_iter().map(|x| if !x {'.'} else {'#'}).collect();
         println!("{}", buff);
     }
+    println!();
+    println!();
 }
 
-fn get_pixel_value(image: &Vec<Vec<bool>>, row: usize, col: usize) -> u32{
-    let nrows = image.len();
-    let ncols = image[0].len();
+fn get_pixel_value(image: &Vec<Vec<bool>>, row: usize, col: usize, default: bool) -> usize{
+    let nrows = image.len() as i32;
+    let ncols = image[0].len() as i32;
     let mut num = 0;
     let mut offset = 8;
+    let row = row as i32;
+    let col = col as i32;
     for i_row in row-1..row+2{
         for i_col in col-1..col+2{
-            if image[i_row][i_col]{
+            if i_row < 0 || i_col < 0 || i_row >= nrows || i_col >= ncols {
+                num |= (default as usize) << offset;
+            }
+            else if image[i_row as usize][i_col  as usize]{
                 num |= 1 << offset;
             }
             offset -= 1;
