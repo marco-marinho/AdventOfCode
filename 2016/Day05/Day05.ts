5094b742d85ce973fcb6f9b77c6f81c3490ba355
Marco Marinho <marcoobom@gmail.com>
2022-05-08 13:28:50 +0200

    2016 - Day 06

--- 2016/Day05/Day05.ts
+++ 2016/Day05/Day05.ts
@@ -16,23 +16,23 @@ function task_01() {
         }
         current++;
     }
-    console.log('Task 01: ' + password.join(''));
+    console.log('task 01: ' + password.join(''));
 }
 
 function task_02() {
     let data: string = parseLines(__dirname, 'data.txt')[0];
     let password: string[] = [];
     let current = 0;
-    let fill_status: number[] = [];
-    while (fill_status.length < 8) {
+    let status: number[] = [];
+    while (status.length < 8) {
         const buff = data + current.toString();
         const hash = crypto.MD5(buff).toString();
         if (hash.slice(0, 5) === '00000') {
             const pos = parseInt(hash[5]);
-            if(!isNaN(pos) && pos <= 7 && !fill_status.includes(pos)){
+            if (!isNaN(pos) && pos <= 7 && !status.includes(pos)) {
                 password[pos] = hash[6];
-                fill_status.push(pos);
-           }
+                status.push(pos);
+            }
         }
         current++;
     }
